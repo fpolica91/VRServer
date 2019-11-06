bbd2592e2808c7793456c6b7b9396372db5e5b96
= <jacex17@gmail.com>
2019-11-05 20:26:30 -0500

    fix likes

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -29,7 +29,7 @@ postroutes.post('/createNewPost', uploader.single("imageUrl"), (req, res, next) 
 
 postroutes.get('/createNewPost', (req, res, next) => {
   try{ 
-    Post.find().populate('owner', '_id imageUrl username')
+    Post.find().populate('owner', '_id imageUrl username').populate('likes', '_id imageUrl username')
    .then(theData => {res.status(200).json(theData)})
    .catch(err => next(err))
  }catch(err){
@@ -44,40 +44,41 @@ postroutes.get('/createNewPost', (req, res, next) => {
   if(req.body._id === undefined){
     res.json({message: "WRONG!"})
   }else{
-   const theUser = req.body._id
+   const theUserId = req.body._id
    const postId = req.params.id
-   Post.findById(postId)
+   Post.findById(postId).populate('likes', '_id imageUrl username')
    .then(thePost => {
-     //CHECK IF THE USER IS ALREADY IN THE LIKE ARRAY
-     if(thePost.likes.indexOf(theUser) >= 0){
-       const theIndex = thePost.likes.indexOf(theUser)
-       thePost.likes.splice(theIndex, 1);
-       thePost.save((err)=>{
+     User.findById(theUserId).select('_id imageUrl username')
+     .then(theUser => {
+       console.log(theUser._id)
+   //CHECK IF THE USER IS ALREADY IN THE LIKE ARRAY
+   console.log(thePost.likes.findIndex(userToFind => userToFind.id === theUser.id));
+      const theIndex = thePost.likes.findIndex(userToFind => userToFind.id === theUser.id)
+      if(theIndex >= 0){
+        thePost.likes.splice(theIndex, 1);
+        thePost.save((err)=>{
+         if(err){
+           res.json({success: false, message: "Something went wrong while Liking the post"})
+         }else{
+           res.json(thePost)
+         }
+       })
+ // IF THE USER IS NOT IN THE ARRAY, WE PUSH THE USER TO THE ARRAY
+      }else{
+      thePost.likes.push(theUser);
+      thePost.save((err)=>{
         if(err){
           res.json({success: false, message: "Something went wrong while Liking the post"})
         }else{
           res.json(thePost)
         }
-      })
-// IF THE USER IS NOT IN THE ARRAY, WE PUSH THE USER TO THE ARRAY
-     }else{
-     thePost.likes.push(theUser);
-     thePost.save((err)=>{
-       if(err){
-         res.json({success: false, message: "Something went wrong while Liking the post"})
-       }else{
-         res.json(thePost)
-       }
      })
-    }
-    
-  //    res.json(thePost)
-  //  }).catch(err => {
-  //    res.json(err);
-  
-   })
+
+      }
+    }).catch(err => console.log(err))
+   }).catch(err => console.log(err))
   }
- })
+})  
 
  //EDIT POST ROUTE
  postroutes.put('/updatePost/:id', async (req, res, _) => {
